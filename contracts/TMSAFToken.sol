256203f54d043562a864a8650a0364436039db91
ViacheslavMiasoiedov <134439944+ViacheslavMiasoiedov@users.noreply.github.com>
2023-06-13 22:14:51 +0300

    initial commit

---
+++ contracts/TMSAFToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import {IERC20} from "@openzeppelin/contracts/token/IERC20/IERC20.sol";
+
+contract TMSAFToken is IERC20 {
+    
+}
